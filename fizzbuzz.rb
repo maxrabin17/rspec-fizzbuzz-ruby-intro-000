a59e2206734d94f64503b41e2b0a41a7202fe7b6
Max Rabin <max.rabin17@gmail.com>
2021-02-14 17:49:04 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+
+  if int % 3 == 0 && int % 5 == 0
+    puts "#{int}. Fizzbuzz"
+  elsif int % 3 == 0
+    puts "#{int}. Fizz"
+  elsif int % 5 == 0
+    puts "#{int}. Buzz"
+  else
+    puts "#{int}"
+  end
+
+end
